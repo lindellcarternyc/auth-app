6accd042998c0f8ce36ef925c5b823cae27d5c50
lindellcarternyc <lindellcarternyc@gmail.com>
2020-10-03 14:32:22 -0400

    update base button

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 interface BaseButtonProps {
   children: React.ReactNode
   onClick?: () => void
   className?: string
 }
+
+const BaseButtonWrapper = styled.button`
+  background: transparent;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  cursor: pointer;
+  margin: 0;
+  padding: 0;
+`
 
 const BaseButton: React.FC<BaseButtonProps> = ({ children, onClick, className }) => {
   const handleClick = (evt: React.SyntheticEvent<HTMLButtonElement>) => {
@@ -15,7 +26,7 @@ const BaseButton: React.FC<BaseButtonProps> = ({ children, onClick, className })
   }
 
   return (
-    <button onClick={handleClick} className={className}>{children}</button>
+    <BaseButtonWrapper onClick={handleClick} className={className}>{children}</BaseButtonWrapper>
   )
 }
 
